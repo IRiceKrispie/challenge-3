1551133b4bb83471684dd7ee7ecc5a7ee63c5f7d
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2022-09-28 06:37:59 -0500

    added boolean variables to check if user wants a certain type of character

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,18 @@ var generateBtn = document.querySelector("#generate"); //locate our button using
 
 //this is our main function that will generate our password 
 function generatePassword(){
+  //initiate our variables and our arrays with our special characters
+  var passwordBuilder = "";
+  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialCharcters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+  var capAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+  //boolean variables to check if the user wants this type of character or not. They all start as false.
+  var ifLowerCase = false;
+  var ifSpecialCharacters = false;
+  var ifCapAlphabet = false;
+  var ifNumbers = false
 }
 
 //this function gets our generated password and uses it as text
